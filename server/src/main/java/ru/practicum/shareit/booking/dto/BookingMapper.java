b969fecd6f88153c891856066055c429a63c86d8
Ситников Сергей <sssitnikov@inbox.ru>
2026-07-12 23:16:37 +0300

    Выполнил задание 16 спринта

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
 import ru.practicum.shareit.booking.Booking;
-import ru.practicum.shareit.booking.BookingStatus;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 
