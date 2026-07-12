edbd336a2812aa06d4f94845e3754e3c4b11f0c0
Ситников Сергей <sssitnikov@inbox.ru>
2026-07-12 19:44:45 +0300

    Выполнил задание 16 спринта

--- server/src/test/java/ru/practicum/shareit/request/dto/ItemRequestMapperTest.java
+++ server/src/test/java/ru/practicum/shareit/request/dto/ItemRequestMapperTest.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request.dto;
 
 import org.junit.jupiter.api.Test;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
